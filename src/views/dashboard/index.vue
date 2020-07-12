a236e90e182a2614c7fcf0c4f59301a642261521
zhengfeizhang <zhengfeizhang@sina.com>
2020-07-12 23:03:11 +0800

    首页统计开发

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -29,7 +29,20 @@
             <equTypeDefects title="各设备类型缺陷数量" :data="equTypeDefectsData"></equTypeDefects>
             <equTypeDefects title="各厂商缺陷数量" :data="companyDefectsData"></equTypeDefects>
         </div>
-        <mutiBar style="height:45%" title="地市公司设备类型缺陷数量" :departments="departments" :data="departmentStationDefectsData"></mutiBar>
+        <mutiBar
+            style="height:45%"
+            title="各电站设备类型缺陷数量"
+            :legend="legend"
+            :xAxisData="stations"
+            :data="seriesData"
+        ></mutiBar>
+        <mutiBar
+            style="height:45%"
+            title="各电站设备类型缺陷修复数量"
+            :legend="legend"
+            :xAxisData="stations"
+            :data="seriesData2"
+        ></mutiBar>
     </div>
 </template>
 <script>
@@ -37,59 +50,157 @@ import statsApi from '@/api/stats.js';
 import equTypeDefects from './baseBar';
 import mutiBar from './mutiBar';
 export default {
-    name: 'firstPage',
-    components: {
-        equTypeDefects,
-        mutiBar
-    },
-    data() {
-        return {
-            equTypeDefectsData: [],
-            companyDefectsData: [],
-            defectTotalData: [],
-            departmentStationDefectsData: [],
-            departments: []
-        };
-    },
-    computed: {},
-    watch: {},
-    methods: {},
-    created() {
-        statsApi.equTypeDefects().then(res => {
-            this.equTypeDefectsData = [];
-            for (let i of res) {
-                this.equTypeDefectsData.push([i.equTypeName, i.defectCount]);
+  name: 'firstPage',
+  components: {
+    equTypeDefects,
+    mutiBar
+  },
+  data() {
+    return {
+      equTypeDefectsData: [],
+      companyDefectsData: [],
+      defectTotalData: [],
+      legend: [],
+      seriesData: [],
+      seriesData2: [],
+      stations: []
+    };
+  },
+  computed: {},
+  watch: {},
+  methods: {},
+  created() {
+    statsApi.equTypeDefects().then(res => {
+      this.equTypeDefectsData = [];
+      for (let i of res) {
+        this.equTypeDefectsData.push([i.equTypeName, i.defectCount]);
+      }
+    });
+    statsApi.companyDefects().then(res => {
+      this.companyDefectsData = [];
+      for (let i of res) {
+        this.companyDefectsData.push([i.company, i.defectCount]);
+      }
+    });
+    statsApi.defectTotal().then(res => {
+      this.defectTotalData = res;
+    });
+    statsApi.departmentStationDefects().then(res => {
+      res = [
+        {
+          "station": "先锋边",
+          "department": "台州市公司",
+          "equTypeList": [
+            {
+              "equTypeName": "温度传感器",
+              "defectCount": 16,
+              "repairCount": 37,
+              "equTypeId": "BfcebCAf-1bAF-FBCD-786e-a669c385aBf3"
+            },
+            {
+              "equTypeName": "湿度传感器",
+              "defectCount": 57,
+              "repairCount": 4,
+              "equTypeId": "fd785f6e-6f16-f2fa-9Ff8-2F9BD4c82B7A"
             }
-        });
-        statsApi.companyDefects().then(res => {
-            this.companyDefectsData = [];
-            for (let i of res) {
-                this.companyDefectsData.push([i.company, i.defectCount]);
+          ]
+        },
+        {
+          "station": "刚阳变",
+          "department": "台州市公司",
+          "equTypeList": [
+            {
+              "equTypeName": "温度传感器",
+              "defectCount": 73,
+              "repairCount": 75,
+              "equTypeId": "1fA8BC62-91a0-695A-1ecc-Ee3F317ed1Fa"
+            },
+            {
+              "equTypeName": "电压互感器",
+              "defectCount": 29,
+              "repairCount": 77,
+              "equTypeId": "3c8BCC4d-469F-f3eb-B26a-C19CB69c11Ef"
             }
-        });
-        statsApi.defectTotal().then(res => {
-            this.defectTotalData = res;
-        });
-        statsApi.departmentStationDefects().then(res => {
-            this.departments = [];
-            this.departmentStationDefectsData = [];
-            for (let i of res) {
-                this.departments.push(i.department);
-                for (let j of i.stationList) {
-                    for (let k of j.equTypeList) {
-                        let val = [i.department, k.defectCount];
-                        let item = {
-                            name: k.equTypeName,
-                            type: 'bar',
-                            stack: j.station,
-                            data: []
-                        };
+          ]
+        },
 
-                        this.departmentStationDefectsData.push(item);
-                    }
-                }
+        {
+          "station": "浦口边",
+          "department": "南京市公司",
+          "equTypeList": [
+            {
+              "equTypeName": "温度传感器",
+              "defectCount": 16,
+              "repairCount": 37,
+              "equTypeId": "BfcebCAf-1bAF-FBCD-786e-a669c385aBf3"
+            },
+            {
+              "equTypeName": "电压传感器",
+              "defectCount": 57,
+              "repairCount": 4,
+              "equTypeId": "fd785f6e-6f16-f2fa-9Ff8-2F9BD4c82B7A"
             }
-        });
-    }
+          ]
+        },
+        {
+          "station": "仙林变",
+          "department": "南京市公司",
+          "equTypeList": [
+            {
+              "equTypeName": "温度传感器",
+              "defectCount": 73,
+              "repairCount": 75,
+              "equTypeId": "1fA8BC62-91a0-695A-1ecc-Ee3F317ed1Fa"
+            },
+            {
+              "equTypeName": "电压互感器",
+              "defectCount": 29,
+              "repairCount": 77,
+              "equTypeId": "3c8BCC4d-469F-f3eb-B26a-C19CB69c11Ef"
+            }
+          ]
+        }
+      ]
+      this.stations = [];  // x-data
+      this.seriesData = [];
+      this.legend = [];
+      let _legend = new Set()
+      let _seriesData = new Map();
+      for (let j of res) {
+        this.stations.push(j.station);
+        for (let k of j.equTypeList) {
+          _legend.add(k.equTypeName)
+          let key = `${k.equTypeName}`
+          if (_seriesData.has(key)) {
+            let val = _seriesData.get(key)
+            val.defectData.push([j.station, k.defectCount])
+            val.repairData.push([j.station, k.repairCount])
+            _seriesData.set(key, val)
+          } else {
+            _seriesData.set(key, {
+              name: k.equTypeName,
+              type: 'bar',
+              stack: '总数',
+              defectData: [[j.station, k.defectCount]],
+              repairData: [[j.station, k.repairCount]]
+            });
+          }
+
+        }
+      }
+      // 格式类型转换
+      this.legend = Array.from(_legend);
+      for (let l of _seriesData.keys()) {
+        let item = _seriesData.get(l)
+        let item1 = JSON.parse(JSON.stringify(item))
+        let item2 = JSON.parse(JSON.stringify(item))
+        item1.data = item1.defectData
+        item2.data = item2.repairData
+        this.seriesData.push(item1)
+        this.seriesData2.push(item2)
+      }
+
+    });
+  }
 };
 </script>
--- src/views/dashboard/mutiBar.vue
+++ src/views/dashboard/mutiBar.vue
@@ -10,88 +10,89 @@
 <script>
 import ChartMixin from '@/mixins/resize.js';
 export default {
-    name: 'basebar',
-    mixins: [ChartMixin],
-    props: {
-        title: {
-            type: String,
-            default: ''
-        },
-        departments: {
-            type: Array,
-            default: () => {
-                return [];
-            }
-        },
-        data: {
-            type: Array,
-            default: () => {
-                return [];
-            }
-        }
+  name: 'basebar',
+  mixins: [ChartMixin],
+  props: {
+    title: {
+      type: String,
+      default: ''
     },
-    data() {
-        return {
-            loading: true,
-            echartsDomId: 'mybar'
-        };
+    legend: { type: Array, default: () => { return [] } },
+    xAxisData: {
+      type: Array,
+      default: () => {
+        return [];
+      }
     },
-    computed: {},
-    watch: {
-        // 数据更新重新画图
-        data: function() {
-            this.$chartMixinInitChart(this.getOption());
-        }
-    },
-    methods: {
-        getOption() {
-            let _legend = [];
-            for (let i of this.data) {
-                _legend.push(i.name);
-            }
-            return {
-                title: {
-                    text: this.title
-                },
-                tooltip: {
-                    trigger: 'axis',
-                    axisPointer: {
-                        // 坐标轴指示器，坐标轴触发有效
-                        type: 'shadow' // 默认为直线，可选为：'line' | 'shadow'
-                    }
-                },
-                legend: {
-                    data: _legend || [] // 所有设备类型
-                },
-                grid: {
-                    containLabel: true,
-                    left: '3%',
-                    right: '4%',
-                    bottom: '3%'
-                },
-                xAxis: [
-                    {
-                        type: 'category',
-                        data: this.departments || [] // 地市公司
-                    }
-                ],
-                yAxis: [
-                    {
-                        type: 'value'
-                    }
-                ],
-                series: this.data
-                //  {
-                //     name: '设备类型名称',
-                //     type: 'bar',
-                //     stack: 'xxx电站',
-                //     data: [120, 132, 101, 134, 90, 230, 210]
-                // },
-            };
-        }
-    },
-    mounted() {
-        this.$chartMixinInitChart(this.getOption());
+    data: {
+      type: Array,
+      default: () => {
+        return [];
+      }
     }
+  },
+  data() {
+    return {
+      loading: true,
+      echartsDomId: 'mybar'
+    };
+  },
+  computed: {},
+  watch: {
+    // 数据更新重新画图
+    data: function () {
+      console.log(JSON.stringify(this.getOption()))
+      this.$chartMixinInitChart(this.getOption());
+    }
+  },
+  methods: {
+    getOption() {
+      return {
+        title: {
+          text: this.title
+        },
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            // 坐标轴指示器，坐标轴触发有效
+            type: 'shadow' // 默认为直线，可选为：'line' | 'shadow'
+          }
+        },
+        legend: {
+          textStyle: {
+            color: 'white'
+          },
+          data: this.legend || [] // 所有设备类型
+        },
+        grid: {
+          containLabel: true,
+          left: '2%',
+          right: '2%',
+          bottom: '2%'
+        },
+        xAxis: [
+          {
+            type: 'category',
+            data: this.xAxisData
+          }
+        ],
+        yAxis: [
+          {
+            type: 'value',
+          }
+        ],
+        series: this.data
+        //  {
+        //     name: '设备类型名称',
+        //     type: 'bar',
+        //     stack: 'xxx电站',
+        //     data: [120, 132, 101, 134, 90, 230, 210]
+        // },
+      };
+    }
+  },
+  mounted() {
+    this.$chartMixinInitChart(this.getOption());
+  }
 };
 </script>
